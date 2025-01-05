f6e7f36d9542f5d3002d9df8ebb9c29c79f9bf54
Mayday46 <winstonnlinn@gmail.com>
2025-01-04 19:03:20 -0500

    Finished Task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+    const [inputText, setInputText] = useState("");
+
+    function handleChange(event) {
+        setInputText(event.target.value);
+        // console.log(event.target.value);
+    }
+
+    function onClick() {
+        console.log("Clicked");
+    }
+
     return (
         <div className="container">
         <div className="heading">
             <h1>To-Do List</h1>
         </div>
         <div className="form">
-            <input type="text" />
-            <button>
+            <input onChange = {handleChange}type="text" value = {inputText}/>
+            <button onClick = {onClick}>
             <span>Add</span>
             </button>
         </div>
